ca77e0bc56a093e17e07de68a65941a65e9f95ec
PauloAlves8039 <pj.alves547@gmail.com>
2019-12-01 10:41:29 -0300

    Adicionando a classe Contato

---
+++ AgendaContatos/Entities/Contato.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Arquivo: classe Contato
+ * Autor: Paulo Alves
+ * Descrição: responsável por conter propriedades da entidade Contato
+ * Data: 01/12/2019
+*/
+
+using System;
+
+namespace AgendaContatos.Entities
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Email { get; set; }
+        public int Telefone { get; set; }
+    }
+}
